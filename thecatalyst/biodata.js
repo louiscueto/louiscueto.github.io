f0614c5d413bdd78744484c10ea38384a7d604f4
Louis Isaiah R. Cueto <cuetolouis@yahoo.com>
2021-02-04 16:52:00 +0800

    thecatalyst

---
+++ thecatalyst/biodata.js
@@ -0,0 +1,258 @@
+BIODATA = {
+	"Sphere_fracturepart3": {
+		headerbg: "assets/biodata/PBR.png",
+		idtag: "assets/idtags/PBR.png",
+		first_name: "Patrick",
+		title: "Patrick Reyes",
+		position: "Managing Partner",
+		subtitle: "An advisor and partner of leaders in pursuit of a better leadership approach and better systems that thrive in complexity",
+		someone: {
+			column1: [
+				"draws from a wide range of experience and learning to provide insights in complex situations and challenges",
+				"blends theory and execution",
+				"thrives in change and complexity",
+				"is passionate about motivating and developing people",
+			],
+			column2: [
+				"identifies leverage and emerging risk points in systems of strategic importance",
+				"facilitates the engagement of many diverse perspectives on key issues that leads to effective action",
+				"is experienced in operating in start-up, conglomerate, single-business, multinational and global consulting settings",
+			],
+		},
+		wish: "Patrick wants to share his knowledge, successes and failures with open-minded people to encourage self-awareness and purposefulness. His desire is to be choiceful with whom and on what he spends his energy. Patrick seeks to always act with courage and conviction.",
+		capabilities: [
+			"C-Level General Management",
+			"Strategy Development and Execution",
+			"Culture and Organizational Design",
+			"Mentoring and Coaching Leaders",
+			"Advising Boards and CEOs"
+		],
+		qualifications: {
+			column1: [
+				"Harvard Business School (Boston, MA, USA)",
+				"1998 to 2000",
+				"Master of Business Administration"
+			],
+			column2: [
+				"Boston College (Chestnut Hill, MA, USA)",
+				"1990 to 1994",
+				"Bachelor of Science in Management",
+				"Summa Cum Laude"
+			],
+		},
+		career_history: "<p>Before Catalyst Asia, Patrick served as the Managing Director and Head of Asia Pacific for the Research and Development Group of UnitedHealth Group (UGH).</p>\
+			<p>Prior to UGH, Patrick led AboitizLand, the Real Estate busienss of Aboitiz Equity Ventures (AEV), as its President and Chief Operating Officer. Before Real Estate, Patrick served as the Chief Strategy Officer of AEV which is one of the largest diversified conglomerates in the Philippines with businesses in Power, Banking, Food, Industries and Real Estate.</p>\
+			<p>When Patrick came back full-time to the Philippines in 2007, he started and managed a family-owned Business Process Outsourcing firm called QBitron. QBitron gave clients opportunities to design, test and operate Philippine-based lead generation, lead qualifying and sales closing teams.</p>\
+			<p>While in Boston, Patrick ran sales, marketing, fulfillment and customer service operations as the Chief Operating Officer of a Boston-based start-up in the Healthcare space called Citizens Health. He also worked for six years in the Boston headquarters of Bain &amp; Company, a global management consulting firm, where he led teams in different types of projects, ranging from Growth, Restructuring, Profit Optimization to Re-engineering. He left Bain &amp; Company to join his mentor who was chosen by the investors of Citizens Health to be its CEO.</p>",
+		email: "patrick@thecatalyst.asia",
+		contact: "PH: +63 917 863 3188"
+	},
+
+	"Sphere_fracturepart20": {
+		headerbg: "assets/biodata/CDS.png",
+		idtag: "assets/idtags/CDS.png",
+		first_name: "Cliff",
+		title: "Cliff Scott",
+		position: "Partner",
+		subtitle: "A partner, guide and coach focused on helping leaders scale their leadership capabilities and effectiveness, enabling them to thrive in the midst of change and complexity",
+		someone: {
+			column1: [
+				"draws from a wide range of experience and learning to equip leaders with the ability to move beyond their existing perspectives and mindsets to embrance and utilize a wider range of competencies",
+				"works to support effective application of new understanding to achieve practical and significant outcomes",
+				"supports leaders to become more self-aware, more authentic in their communications, relationships and actions",
+			],
+			column2: [
+				"is passionate for each client's opportunity to grow and flourish professionally and personally",
+				"is experienced in operating in start-up, conglomerate, single-business, multinational and global consulting settings",
+			],
+		},
+		wish: "Cliff wants to partner with those who seek to grow beyond their limitations and to become the kind of leader they aspire to be. His commitment is to bring compassion and empathy, creating a safe space for leaders to examine their beliefs, assumptions, strengths and potential; to do the hard work of letting go of what is not serving them and to experiment and build new strength in what will. His desire is to invest his time and energy with those who will engage in self-discovery.",
+		capabilities: [
+			"Consultant, coach and trusted advisor to leaders at all levels, including Boards",
+			"Developmental program and training design and delivery",
+			"Culture and Organizational Development",
+			"Advising Boards and CEOs"
+		],
+		qualifications: {
+			column1: [
+				"Boston College (Chestnut Hill MA, USA)",
+				"1976 to 1980",
+				"Master in Social Planning and Development",
+				"<br>",
+				"University of Massachusetts (Amherst MA, USA)",
+				"1969 to 1973",
+				"Bachelor of Human Relations Education",
+				"Summa Cum Laude"
+			],
+			column2: {
+				title: "Certifications:",
+				certifications: [
+					"Growth Edge and Presence-based COaching",
+					"The Leadership Circle system for leader assessment and development",
+					"Myers-Briggs Type Indicator (MBTI)",
+					"Six Sigma quality improvement methodology"
+				]
+			},
+		},
+		career_history: "<p>Cliff is now a global consultant and coach serving companies in diverse fields of technology, financial services, power, food production, among others.</p>\
+			<p>His consultancy affilliations include Cultivating Leadership, THe Leadership Circle, as well as Catalyst Asia, where he focuses on SE Asian clients. He has been in private consulting since 1992, largely focused on US clients and in some cases those that were multinational. His experience has led to deep expertise and experience with companies in manufacturing, agribusiness, utilities, and computer/digital technologies. Cliff has also worked with governmental entities, such as the US Navy, Army Corps of Engineers, and the US Patent Office. He has also worked with NGO's and social service organizations.</p>\
+			<p>His work has included training and facilitation to development leadership and management skills, coaching and mentoring skills, communication skills. His consulting work has included quality improvement but mostly focused on building strong and aligned intact teams of leaders facing strategic changes and challenges. Typically, this has focused on very senior and top leaders.</p>\
+			<p>Prior to private consulting, Cliff held leadership roles as an internal Organization Development Consultant and Human Resources. He spent 11 years across three organizations, Tufts University (Medford, MA, USA), Houghton Mifflin Publishing (Boston, MA USA), and Bolt, Beranik and Newman (BBN) (Cambridge, MA USA).</p>",
+		email: "cliff@cultivatingleadership.com",
+		contact: "PH: +63 917 803 3419 | US: +1 425 244 2250"
+	},
+
+	"Sphere_fracturepart19": {
+		headerbg: "assets/biodata/VVL.png",
+		idtag: "assets/idtags/VVL.png",
+		first_name: "Vivien",
+		title: "Vivien Limjoco",
+		position: "Director",
+		subtitle: "A change agent and operator that steer people and leverages systems to outperform",
+		someone: {
+			column1: [
+				"looks for environments of mutual learning",
+				"always seeks ways to push for optimal outcomes",
+				"grounds assessments in facts"
+			],
+			column2: [
+				"is relentless and tireless in executing improvement plans",
+				"is comfortable being a pitfall revealer in a room full of optimists",
+				"is constantly curious about what else can be done to add value"
+			],
+		},
+		wish: "Vivien seeks to be always adding value to her own life and to those of others. She wants to be enganged and continuously learning.",
+		capabilities: [
+			"Change Management",
+			"Project Management",
+			"Corporate Planning",
+			"Corporate Budgeting",
+			"Process Performance Management"
+		],
+		qualifications: {
+			column1: [
+				"European Business SChool (London, UK)",
+				"Bachelor of Arts (Hons) in International Business Majors in Corporate Finance and International Economics",
+				"2nd Division Honours"
+			],
+			column2: {
+				title: "Certifications:",
+				certifications: [
+					"Leading Operational Excellence (MIT Sloan Executive Education)",
+					"Accelerating Change Readiness and Agility (UC Berkeley Haas School of Executive Education)",
+					"Balanced Scorecard (The Balanced Scorecard Institute)"
+				]
+			},
+		},
+		career_history: "<p>Before joining Catalyst Asia, Vivien served as the CFO of Bridge Southeast Asia, a start-up providing HR and finance solutions to companies and their employees.</p>\
+			<p>Vivien spent many years in different roles and companies in the Aboitiz Group. She started in the shipping and logistics business which was eventually known as 2GO where she headed Corporate Planning and Investor Relations as part of the Office of the CFO. After the Aboitizes divested 2GO, Vivien moved to the conglomerates holding company, Aboitiz Equity Ventures (AEV), as the Assistant Vice President for Strategy and Corporate Finance before taking on general management roles in AboitizLand, the Real Estate company of AEV. In AboitizLand, Vivien headed the Project Development department in AboitizLand before being promoted to handle on of the three business units of the company as Residential Business Unit Head.</p>\
+			<p>Vivien started her business career in finance in UBP's Privatization and Equities Group and as a Senior Associate in PCI Capital Corporation. She also served as the Engagement Director of AJOnet Holdings before joining the Aboitiz Group.</p>",
+		email: "vivien@thecatalyst.asia",
+		contact: "PH: +63 917 727 8528"
+	},
+
+	"Sphere_fracturepart18": {
+		headerbg: "assets/biodata/CSM.png",
+		idtag: "assets/idtags/CSM.png",
+		first_name: "Charity",
+		title: "Charity Marohombsar",
+		position: "Director",
+		subtitle: "A leader and coach that pushes through multifaceted challenges to achieve organizational improvements and optimal returns by solving complex issues",
+		someone: {
+			column1: [
+				"has an unwavering commitment to success and a focus on delivering value to customers and shareholders",
+				"offers a rare ability to develop good relationships with prospective clients  and balance business requirements while maintaining a highly strategic, big-picture perspective"
+			],
+			column2: [
+				"inspires and motivates people from all cultures to a common cause",
+				"is authentically keen in developing individuals and teams who are not just willing but are committed to their self-growth"
+			],
+		},
+		wish: "Charity seeks to be a co-explorer of individuals and leaders who are committed to be better versions of themselves. She intends to continuously align her passion and her purpose.",
+		capabilities: [
+			"Leadership Coaching and Mentoring",
+			"Customer Experience Management",
+			"Business Development",
+			"Process Management and Innovation",
+			"Sales and Marketing and Profitable Growth"
+		],
+		qualifications: {
+			column1: [
+				"Ateneo de Naga University (Naga, Philippines)",
+				"1984 to 1987",
+				"Bachelor of Arts in English",
+				"Major in Literature"
+			],
+			column2: {
+				title: "Certifications:",
+				certifications: [
+					"Leadership Circle Profile (The Leadership Circle)",
+					"Tetra Map Facilitation (Tetra Map)",
+					"Points of VIew (Convergent Experience)",
+					"Coaching (John Maxwell)",
+					"Training (John Maxwell)",
+					"Customer Comes First (Guthrie Jensen)"
+				]
+			},
+		},
+		career_history: "<p>Charity's career has been a progressive track of general management and leadership in large corporate settings.</p>\
+			<p>Charity's general manager journey culminated as the Business Unit Head of AboitizLand's Industrial Real Estate Division where she managed PEZA parks in Luzon and Visayas. She made a mark in other parts of the Aboitiz Group as she played senior management roles in 2GO, the Shippping & Logistics company which used to be the flagship business of the Aboitizes. She entered the 2GO business as Vice President of its RORO business, then subsequently held other Vice President and COO positions in Freight Sales & Marketing, Customer Care and ScanAsia.</p>\
+			<p>Charity also had leadership roles in other sectors as she held positions such as General Manager of Branded Lifestyle, a retail and distribution market leader in eyewear and fashion accessories, and General Manager and Vice President of Source One Asia (a BPO firm) and Eastern Telecoms, respectively.</p>\
+			<p>Charity started her management track by accumulating skills and knowledge across functional disciplines. She worked in HR as a consultant for John Clements, cut her teeth in Sales as the Corporate Advertising as a Call Center Manager in the Communications Group of the Lopez Group of Companies.</p>\
+			<p>Charity now leverages her experiences as a general manager to be a coach to senior leaders and a mentor to managers who seek to evolve their leadership approach.</p>",
+		email: "charity@thecatalyst.asia",
+		contact: "PH: +63 917 711 0105"
+	},
+
+	"Sphere_fracturepart11": {
+		headerbg: "assets/biodata/MG.png",
+		idtag: "assets/idtags/MG.png",
+		first_name: "Mikha",
+		title: "Mikhaellah Gillego",
+		position: "Manager",
+		subtitle: "A leader and change agent that seeks to continuously learn and adapt to prosper in environments that are unpredictable and constantly changing",
+		someone: {
+			column1: [
+				"has been exposed to an array of situations that provide her with an understanding of how to tackle many operational challenges",
+				"recognizes opportunities for improvement and finds ways to realize them"
+			],
+			column2: [
+				"is committed to developing programs that can make positive changes in an organization",
+				"is a people-centric leader that builds strong relationships and creates trust in an open environment"
+			],
+		},
+		wish: "Mikha wants to inspire and influence people she touches to have a positive mindset even as they go through challenging times. Mikha wants to always strive to find the nuggets of learning in every experience she goes through",
+		capabilities: [
+			"Operations Management",
+			"Process Design",
+			"Training Program Devlopment",
+			"Performance Management",
+			"Customer Management"
+		],
+		qualifications: {
+			column1: [
+				"San Sebastian College (Manila, Philippines)",
+				"2008 to 2012",
+				"Bachelor of Science",
+				"Major in Hotel and Restaurant Management"
+			],
+			column2: {
+				title: "Certifications:",
+				certifications: [
+					"Effective Management Practices",
+					"People Management",
+					"Advance Shift Management"
+				]
+			},
+		},
+		career_history: "<p>Prior to joining Catalyst Asia, Mikha learned about managing people and improving systems in the Food & Beverage serctor.</p>\
+			<p>The foundation of her knowledge was laid in the rigorous and disciplined environment of McDonald's where she rose from trainee to Assistant Manager in two years.</p>\
+			<p>Mikha took her learnings in McDonald's to Wee Nam kee, where she was able to redesign the inventory system and training programs that created measurable improvements in less than a year.</p>\
+			<p>She grabbed a chance to expand her skill set by shifting from Store Operations to Sales with the Bistro Group of Companies.</p>\
+			<p>She then joined Specialty Food Retailers when an opportunity came up to lead both Front-of-the-House and Back-of-the-House operations of a TWG branch. In TWG, Mikha was able to redesign training programs and create sales tools that were deployed in her branch and other branches under her Area Manager.</p>\
+			<p>Mikha now deepends her knowledge in the fields of Leadership and Organizational Development as she collaborates with Catalyst Asia teammates and clients.</p>",
+		email: "mikha@thecatalyst.asia",
+		contact: "PH: +63 917 145 1681"
+	},
+
+}
---
+++ thecatalyst/modal.php
@@ -0,0 +1,240 @@
+<div class="modal" id="modal">
+	
+
+	<style type="text/css">
+		.modal-content-header {
+			margin: 0; padding-bottom: 1em; border-bottom: 2px solid white;
+		}
+		#modal-header {
+			padding: 5em 3em;
+			background-color: #cacaca;
+			background-image: url('assets/biodata/PBR.png');
+			background-size: cover;
+			background-repeat: no-repeat;
+		}
+		#modal-title {
+			font-size: 2.75rem;
+			padding-bottom: .25em;
+			margin-bottom: .25em;
+			border-bottom: 2px solid white;
+		}
+		#modal-subtitle {
+			font-size: 1rem;
+			letter-spacing: 1px;
+			font-weight: bold;
+			margin-bottom: 0;
+		}
+		.flex {
+			display: flex;
+		}
+		.flex-col {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+		}
+		.modal-header-left {
+			width: 80%;
+			text-align: center;
+			padding: 2.5em 3em;
+		}
+		.modal-header-right {
+			width: 20%;
+			padding: 2em;
+		}
+	</style>
+	<div class="modal-content noto">
+		<button class="close">X</button>
+		<div id="modal-header">
+			<div class="flex">
+				<div class="modal-header-left">
+					<h1 class="roboto" id="modal-title">MIKHAELLAH GILLEGO</h1>
+					<p class="poppins" id="modal-subtitle">A leader and change agent that seeks to continuously learn and adapt to prosper in environments that are unpredictable and constantly changing.</p>
+				</div>
+				<div class="modal-header-right flex-col"></div>
+			</div>
+		</div>
+		<div style="padding: 2em 5em; background-color: #e5e7e8; display: none;">
+			<div style="display: flex; margin-bottom: 2em;">
+				<div style="width: 40%; text-align: right; padding: 0 2em 0 0; border-right: 2px solid white;">
+					<h1 class="roboto" style="line-height: 1.1;">THE<br>CATALYST<br>ASIA'S<br>PERSPECTIVE<br>ON ITS ROLE</h1>
+				</div>
+				<div class="poppins" style="width: 60%; padding: .85em 2em .85em 2em; font-weight: bold; letter-spacing: 1.2px;">
+					<p style="font-size: .85rem;">Leaders need to evolve their way of thinking and understanding issues of increasing complexity in their businesses and in the contexts their businesses operate in. This is a growing imperative for them to remain competitive and certainly if they intend to grow.</p>
+					<p style="font-size: .85rem;">Companies' systems and processes are more typically designed for a very predictable world. Increasingly the complex challenges businesses face are not predictable, often quite abruptly so, and the unfolding challenges are often ambiguous as well.</p>
+					<p style="font-size: .85rem;">It is not enough for leaders to improve just their leadership capabilities, they also need to simultaneously change how their organizations are designed and operate to thrive and eventually excel in complexity. This change involves modifying norms and behaviors, changing policies and procedures, and implementing programs, systems and technologies that can enable and support these modifications.</p>
+					<p style="font-size: .85rem;">The Catalyst Asia group can advise and partner with leaders who want to transform their leadership approach to make themselves in to leaders who can thrive in complexity. We also advise and partner with leaders who are also ready to change their organizations to become complexity friendly as well.</p>
+					<p style="font-size: .85rem;">Catalyst Asia partners with leaders to first assess their existing leadership capabilities to determine how adaptive they are in the face of complex challenges. Catalyst Asia can then guide leaders or leadership teams to change their leadership mindset, approach and dynamics to be optimally suited for their complex environment.</p>
+					<p style="font-size: .85rem;">Catalyst Asia also supports leaders to apply their newly realized ways of leadership to how their companies are organized and operate, readying the organization to succeed in complexity. Support typically takes the form of diagnosing the organization's needed design changes and facilitating change programs to position their companies to execute in ways adapted to rapid and/or unexpected change, evolving customer expectations and needs, shifting employee demographics, new technologies, disruptive innovations, and regional or global competition.</p>
+				</div>
+			</div>
+		</div>
+		<style type="text/css">
+			.someone-who td {
+				padding: 0;
+				margin: 0;
+				vertical-align: top;
+				font-weight: bold;
+			}
+			.someone-who td:first-child {
+				padding-right: 1.5em;
+			}
+			.someone-who td:last-child {
+				padding-left: 1.5em;
+			}
+			.someone-who ul {
+				padding-left: 0;
+			}
+			.someone-who li {
+				letter-spacing: 0.85px;
+				font-size: 0.75rem;
+				margin-bottom: .5em;
+			}
+			#someone-who {
+				width: 100%; table-layout: fixed;
+			}
+		</style>
+		<div style="padding: 2em 6em;">
+			<div style="padding: 1.5em 4em;  background-color: #c1c0c0;">
+				<h3 class="roboto" style="margin: 0;" id="modal-someone-header">MIKHA IS SOMEHONE WHO...</h3>
+			</div>
+			<div style="padding: 0 4em 1em; background-color: #cacaca;">
+				<table class="someone-who poppins" id="someone-who">
+					<tr>
+						<td>
+							<ul>
+								<li>has been exposed to an array of situations that provide her with an understanding of who to tackle many operational challenges</li>
+								<li>recognizes opportunities for improvement and finds ways to realize them</li></ul>
+							</ul>
+						</td>
+						<td>
+							<ul>
+								<li>is committed to developing programs that can make positive changes in an organization</li>
+								<li>is a people-centric leader that builds strong relationships and creates trust in an open environment</li>
+							</ul>
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	
+
+		<style type="text/css">
+			#wish {
+				margin: 0;
+				letter-spacing: 0.85px;
+				font-size: 0.75rem;
+				font-weight: bold;
+			}
+		</style>
+		<div style="padding: 2em 6em;">
+			<div style="padding: 1.5em 4em 1em; background-color: #cacaca; ">
+				<h3 class="roboto modal-content-header" id="modal-wish-header">MIKHA'S WISH FOR THE FUTURE</h3>
+			</div>
+			<div style="padding: 0 4em 2em; background-color: #cacaca;">
+				<p class="poppins" id="wish">Mikha wants to inspire and influence people she touches to have a positive mindset even as they go through challenging times. Mikha wants to always strive to find the nuggets of learning in every experience she goes through.</p>
+			</div>
+		</div>
+
+		<style type="text/css">
+			.capabilities ul {
+				padding-left: 0;
+				padding-right: 1em;
+			}
+			.capabilities ul > li {
+				letter-spacing: .85px;
+				font-size: 0.85rem; font-weight: bold;
+				margin-bottom: .5em;
+			}
+		</style>
+		<div style="padding: 2em 6em;">
+			<h3 class="roboto modal-content-header" style="width: 85%;">CAPABILITIES</h3>
+			<div class="poppins capabilities" style="padding: 1em 4em 1em; font-size: 0.75rem;" id="capabilities">
+				<div style="display: flex;">
+					<div>
+						<ul><li>Operations Management</li><li>Training Program Development</li></ul>
+					</div>
+					<div style="margin-left: auto;">
+						<ul><li>Process Design</li><li>Performance Management</li></ul>
+					</div>
+				</div>
+				<div style="text-align: center;">
+					<ul style="padding-left: 0; width: fit-content; margin: auto;"><li>Customer Management</li></ul>
+				</div>
+			</div>
+		</div>
+
+		<style type="text/css">
+			#qualifications-1 {
+				padding-right: 1em;
+			}
+		</style>
+		<div style="padding: 2em 6em; ">
+			<div style="padding: 1.5em 4em 1em; background-color: #cacaca;">
+				<h3 class="roboto modal-content-header">QUALIFICATIONS</h3>
+			</div>
+			<div style="padding: .25em 4em 1.5em; background-color: #cacaca;">
+				<div class="poppins" style="display: flex; letter-spacing: 0.85px; font-size: 0.75rem; font-weight: bold;">
+					<div id="qualifications-1">
+						San Sebastian College (Manila, Philippines)<br>
+						2008 to 2012<br>
+						Bachelor of Science<br>
+						Major in Hotel and Restaurant Management<br>
+					</div>
+					<div id="qualifications-2" style="margin-left: auto; margin-right: 1em;">
+						<b><i>Certifications:</i></b>
+						<ul style="padding-left: 1em;">
+							<li>Effective Management Practices</li>
+							<li>People Management</li>
+							<li>Advance Shift Management</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<style type="text/css">
+			#career_history {
+				letter-spacing: 0.85px;
+				font-size: 0.75rem;
+				font-weight: bold;
+			}
+		</style>
+		<div style="padding: 2em 10em; margin-bottom: 2em;">
+			<h3 class="roboto modal-content-header">CAREER HISTORY</h3>
+			<div class="poppins" id="career_history">
+				<p>Prior to joining Catalyst Asia, Mikha learned about managing people and improving systems in the Food &amp; Beverage sector.</p>
+				<p>The foundation of her knowledge was laid in the rigorous and disciplined environment of McDonald's where she roes from trainee to Assistant Manager in two years.</p>
+				<p>Mikha took her learnings in McDonald's to Wee Nam Kee, where she was able to redesign the inventory system and training programs that created measurable improvements in less than a year.</p>
+				<p>She grabbed a chance to expand her skill setr by shifting from Store Operations to Sales with the Bistro Group of Companies.</p>
+				<p>She then joined Specialty Food Retailers when an opportunity came up to lead both Front-of-the-House and Back-of-the-House operations of a TWG branch. In TWG, Mikha was able to redesign training programs and create sales tools that were deployed in her branch and other branches under her Area Manager.</p>
+				<p>Mikha now deepens her knowledge in the fields of Leadership and Organizational Development as she collaborates with Catalyst Asia teammates and clients.</p>
+			</div>
+		</div>
+
+		<div style="padding: 2em 10em; text-align: center; margin-bottom: 2em;">
+			<img src="assets/TCA_Logo_FA_Layered.png" style="width: 300px; height: auto; margin: auto;">
+		</div>
+
+		<style type="text/css">
+			.modal-footer {
+				display: flex;
+				padding: 2em 5em;
+				background-color: whitesmoke;
+			}
+			.modal-footer-data {
+				letter-spacing: 1px;
+				font-weight: bold;
+			}
+			#modal-email {
+				text-align: left; padding: 1em 0 1em 5em;
+			}
+			#modal-contact {
+				text-align: right; padding: 1em 5em 1em 0;
+			}
+		</style>
+		<div class="modal-footer">
+			<div class="fw modal-footer-data" id="modal-email">mikha@thecatalyst.asia</div>
+			<div class="fw modal-footer-data" id="modal-contact">PH +63 917 145 1681</div>
+		</div>
+	</div>
+</div>
